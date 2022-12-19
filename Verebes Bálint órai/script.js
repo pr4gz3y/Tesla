09c54f86e51965bfcfb351250db771563918def3
pr4gz3y <112938673+pr4gz3y@users.noreply.github.com>
2022-12-19 10:03:30 +0100

    Add files via upload

---
+++ Verebes Bálint órai/script.js
@@ -0,0 +1,9 @@
+function kepvaltkek(){
+    document.getElementById("img").src = "./pics/m3blue.jpg";
+}
+function kepvaltpir(){
+    document.getElementById("img").src = "./pics/m3red.jpg";
+}
+function kepvaltfeh(){
+    document.getElementById("img").src = "./pics/m3white.jpg";
+}
